3ce765c8728ae74eb2242af8cd5da0ec98139261
Wild Life Studio <96738495+pooriyaYT1374@users.noreply.github.com>
2023-06-26 23:22:15 +0330

    Fixed Music Embed

--- voice/onTrackStart.js
+++ voice/onTrackStart.js
@@ -1,29 +1,29 @@
 module.exports = {
-    type: "trackStart",
-    channel: "$channelID",
-    code: `
-    $setGuildVar[music_channelID;$channelID]
-    $setGuildVar[music_player;$get[id]]
-   $wait[2s]
-  $let[id;$sendMessage[{newEmbed:{author:Now Playing}{title:$songInfo[title]}{description: $songInfo[description]}{description:
-  
-    >  🎵  **Artist:** **$songInfo[artist]**
-  
-    >  🎵  **Duration:** **$digitalformat[$songInfo[duration]]**
-    
-    >  🎵  **Volume:** **$volume**
-    
-    >  🎵  **Url:** **$songInfo[url]**
-    
-    >  🎵  **Requested By:**  **$songInfo[requester]**
-    
-    >  🎵  **Veiws:** **$songInfo[views]**
-    
-    >  🎵  **Platform:** **$songInfo[formatedPlatforms]**
+  type: "trackStart",
+  channel: "$channelID",
+  code: `
+  $setGuildVar[music_channelID;$channelID]
+  $setGuildVar[music_player;$get[id]]
+ $wait[2s]
+$let[id;$sendMessage[{newEmbed:{author:Now Playing}{title:$songInfo[title]}{description:
 
-  }{image:$songInfo[thumbnail]}{color: $getVar[color]}};true]]
-  $suppressErrors
-  $volume[100]
-  $wait[4s]
-      `
-  }
+  >  🎵  **Artist:** **$songInfo[artist]**
+
+  >  🎵  **Duration:** **$digitalformat[$songInfo[duration]]**
+  
+  >  🎵  **Volume:** **$volume**
+  
+  >  🎵  **Url:** **$songInfo[url]**
+  
+  >  🎵  **Requested By:**  **$songInfo[requester]**
+  
+  >  🎵  **Veiws:** **$songInfo[views]**
+  
+  >  🎵  **Platform:** **$songInfo[formatedPlatforms]**
+
+}{image:$songInfo[thumbnail]}{color: $getVar[color]}};true]]
+$suppressErrors
+$volume[100]
+$wait[4s]
+    `
+}
