ee31e6a17145b51da96812584a579ec73634379d
Wild Life Studio <96738495+pooriyaYT1374@users.noreply.github.com>
2023-06-26 23:23:39 +0330

    Update onTrackEnd.js

--- voice/onTrackEnd.js
+++ voice/onTrackEnd.js
@@ -1,13 +1,13 @@
 module.exports = {
-    type: "trackEnd",
-    channel: "$channelID",
-    code: `
-  $deleteMessage[$getGuildVar[music_player];$getGuildVar[music_channelID]]
-  $onlyIf[$channelExists[$getGuildVar[music_channelID]]==true;]
-  $onlyIf[$messageExists[$getGuildVar[music_player]]==true;]
-  
-  $title[🎵 آهنگ به اتمام رسید 🎵 ]
-  $color[Green]
+  type: "trackEnd",
+  channel: "$channelID",
+  code: `
+$deleteMessage[$getGuildVar[music_player];$getGuildVar[music_channelID]]
+$onlyIf[$channelExists[$getGuildVar[music_channelID]]==true;]
+$onlyIf[$messageExists[$getGuildVar[music_player]]==true;]
 
-      `
+$title[🎵 Music End🎵 ]
+$color[Green]
+
+    `
 }
