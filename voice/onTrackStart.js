353a1b7dd776a8a5e461e1bc4995cd3a3f210354
Wild Life Studio <96738495+pooriyaYT1374@users.noreply.github.com>
2023-06-27 11:23:34 +0330

    Version 7.0.5

--- voice/onTrackStart.js
+++ voice/onTrackStart.js
@@ -1,43 +1,43 @@
 module.exports = {
-  type: "trackStart",
-  channel: "$channelID",
-  code: `
-    $setGuildVar[music_channelID;$channelID]
-    $setGuildVar[music_player;$get[id]]
-$wait[2s]
-$let[id;$sendMessage[{newEmbed:{author:Now Playing}{title:$songInfo[title]}{description: $songInfo[description]}{description:
-
-    >  🎵  **Artist:** **$songInfo[artist]**
+    type: "trackStart",
+    channel: "$channelID",
+    code: `
+      $setGuildVar[music_channelID;$channelID]
+      $setGuildVar[music_player;$get[id]]
+  $wait[2s]
+  $let[id;$sendMessage[{newEmbed:{author:Now Playing}{title:$songInfo[title]}{description: $songInfo[description]}{description:
   
-    >  🎵  **Duration:** **$replaceText[$digitalformat[$songInfo[duration]];00:0;;1]**
+      >  🎵  **Artist:** **$songInfo[artist]**
     
-    >  🎵  **Volume:** **$volume** 
-    
-    >  🎵  **Url:** $songInfo[url]
-    
-    >  🎵  **Requested By:**  $songInfo[requester]
-    
-    >  🎵  **Veiws:** **$songInfo[views]** 
-    
-    >  🎵  **Platform:** **$songInfo[formatedPlatforms]**
-}{image:$songInfo[thumbnail]}{color:$getVar[color]}}  
-{actionRow:
-{button:Previous:primary:previous:false:$customEmoji[$getVar[music2_emoji]]}
-    {button:Next:primary:skip:false:$customEmoji[$getVar[music2_emoji]]}
-
-    }
-    {actionRow:
-        {button:Loop:secondary:loop:false:$customEmoji[$getVar[music2_emoji]]}
-        {button:Pause&Play:secondary:play_pause:false:$customEmoji[$getVar[music2_emoji]]}}
-    {actionRow:
-    {button:Auto Play:danger:auto:false:$customEmoji[$getVar[music_emoji]]}
-    {button:Stop:danger:stop:false:$customEmoji[$getVar[no_emoji]]}}
-        {actionRow:
-  {button:Volume Down:success:volume-down:false:$customEmoji[$getVar[vl_up_emoji]]}
-  {button:Volume Up:success:volume-up:false:$customEmoji[$getVar[vl_down_emoji]]}};true]]
-$suppressErrors
-$volume[$getVar[volume]]
-$setUserVar[play_pause;none;$authorID]
-$wait[4s]
-    `
-}
+      >  🎵  **Duration:** **$replaceText[$digitalformat[$songInfo[duration]];00:0;;1]**
+      
+      >  🎵  **Volume:** **$volume** 
+      
+      >  🎵  **Url:** $songInfo[url]
+      
+      >  🎵  **Requested By:**  $songInfo[requester]
+      
+      >  🎵  **Veiws:** **$songInfo[views]** 
+      
+      >  🎵  **Platform:** **$songInfo[formatedPlatforms]**
+  }{image:$songInfo[thumbnail]}{color:$getVar[color]}}  
+  {actionRow:
+  {button:Previous:primary:previous:false}
+      {button:Next:primary:skip:false}
+  
+      }
+      {actionRow:
+          {button:Loop:secondary:loop:false}
+          {button:Pause&Play:secondary:play_pause:false}}
+      {actionRow:
+      {button:Auto Play:danger:auto:false}
+      {button:Stop:danger:stop:false}}
+          {actionRow:
+    {button:Volume Down:success:volume-down:false}
+    {button:Volume Up:success:volume-up:false}};true]]
+  $suppressErrors
+  $volume[$getVar[volume]]
+  $setUserVar[play_pause;none;$authorID]
+  $wait[4s]
+      `
+  }
