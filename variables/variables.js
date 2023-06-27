76209eeeedeb83cfd160752a9ce2e0364b27a9e7
Wild Life Studio <96738495+pooriyaYT1374@users.noreply.github.com>
2023-06-27 11:51:41 +0330

    Version 7.0.5

--- variables/variables.js
+++ variables/variables.js
@@ -9,11 +9,6 @@ module.exports = bot => {
       authorbutton: "0",
       play_pause: "none",
       loop: "none",
-      music_emoji: "music",
-      music2_emoji: "music2",
-      no_emoji: "x_",
-      vl_up_emoji: "wl_Voice1011",
-      vl_down_emoji: "vloume"
 
 
 
